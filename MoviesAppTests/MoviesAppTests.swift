1fadf0d67e40a85286cd1e27eb140efefdeec188
Lucas Daniel Pedroso <lukas_boo@hotmail.com>
2026-04-29 07:41:43 -0300

    Initial Commit

---
+++ MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by Lucas Pedroso on 29/04/26.
+//
+
+import Testing
+@testable import MoviesApp
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
